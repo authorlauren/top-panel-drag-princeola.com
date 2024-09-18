be42869e8f23d7c6989fe77b7a68d1bd0006b0aa
Olajide Otolorin <otomighty@gmail.com>
2024-09-18 13:51:16 +0100

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -2,7 +2,6 @@ import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
 import Gtk from 'gi://Gtk';
 import GObject from 'gi://GObject';
-
 import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 const PanelElementRow = GObject.registerClass({
@@ -12,21 +11,16 @@ const PanelElementRow = GObject.registerClass({
         super({
             title: label,
         });
-
         this._settings = settings;
         this._label = label;
-
         const positionModel = new Gtk.StringList();
         positionModel.append("Left");
         positionModel.append("Right");
-
         this._dropdown = new Gtk.DropDown({
             model: positionModel,
             selected: initialPosition === 'left' ? 0 : 1,
         });
-
         this._dropdown.connect('notify::selected', this._onPositionChanged.bind(this));
-
         this.add_suffix(this._dropdown);
         this.activatable_widget = this._dropdown;
     }
@@ -35,18 +29,15 @@ const PanelElementRow = GObject.registerClass({
         const newPosition = this._dropdown.selected === 0 ? 'left' : 'right';
         let leftElements = this._settings.get_strv('left-elements');
         let rightElements = this._settings.get_strv('right-elements');
-
         // Remove the element from both arrays to ensure no duplicates
         leftElements = leftElements.filter(e => e !== this._label);
         rightElements = rightElements.filter(e => e !== this._label);
-
         // Add the element to the appropriate array
         if (newPosition === 'left') {
             leftElements.push(this._label);
         } else {
             rightElements.push(this._label);
         }
-
         // Update the settings
         this._settings.set_strv('left-elements', leftElements);
         this._settings.set_strv('right-elements', rightElements);
@@ -55,30 +46,38 @@ const PanelElementRow = GObject.registerClass({
 
 export default class PanelOrganizerPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        try {
-            const settings = this.getSettings();
-            const page = new Adw.PreferencesPage();
-            window.add(page);
+        const settings = this.getSettings();
+        
+        // Log the settings to check if they're being retrieved correctly
+        console.log('Left elements:', settings.get_strv('left-elements'));
+        console.log('Right elements:', settings.get_strv('right-elements'));
 
-            const group = new Adw.PreferencesGroup({
-                title: _('Panel Element Positions'), // Assuming _ is your gettext function
-                description: _('Choose the position for each panel element'),
+        const page = new Adw.PreferencesPage();
+        window.add(page);
+
+        const group = new Adw.PreferencesGroup({
+            title: 'Panel Element Positions',
+            description: 'Choose the position for each panel element',
+        });
+        page.add(group);
+
+        const leftElements = settings.get_strv('left-elements');
+        const rightElements = settings.get_strv('right-elements');
+        const allElements = [...new Set([...leftElements, ...rightElements])];
+        console.log('All elements:', allElements);
+
+        if (allElements.length === 0) {
+            // If there are no elements, add a label explaining this
+            const noElementsRow = new Adw.ActionRow({
+                title: 'No panel elements found',
             });
-            page.add(group);
-
-            const leftElements = settings.get_strv('left-elements');
-            const rightElements = settings.get_strv('right-elements');
-            const allElements = [...new Set([...leftElements, ...rightElements])];
-
-            // Consider using a GtkListBox with a GtkListBoxRow for better performance with many elements
+            group.add(noElementsRow);
+        } else {
             allElements.forEach((element) => {
                 const initialPosition = leftElements.includes(element) ? 'left' : 'right';
                 const row = new PanelElementRow(element, initialPosition, settings);
                 group.add(row);
             });
-        } catch (error) {
-            console.error('Error filling preferences window:', error);
-            // Optionally, display an error message to the user
         }
     }
 }
